d33d239ee50ed73227ff44fed2b4d8b98445f9e9
Rodrigmen <roblesminambresrodrigo@gmail.com>
2020-12-10 19:12:35 +0100

    update, cookies work better now

--- codigoPHP/programa.php
+++ codigoPHP/programa.php
@@ -6,21 +6,13 @@ if (!isset($_SESSION['usuarioDAW218LogInLogOutTema5'])) { //si la sesión no tie
 //BOTONES
 if (isset($_POST['salir'])) { //si ejecutamos el boton 'salir'
     session_destroy(); //Destrucción de la sesión
-    //Destrucción de todas las cookies (esto es para que se borre la cookie creada al iniciar sesión)
-    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
-    foreach ($cookies as $cookie) {
-        $parts = explode('=', $cookie);
-        $name = trim($parts[0]);
-        setcookie($name, '', time() - 1000);
-        setcookie($name, '', time() - 1000, '/');
-    }
     header('Location: ../login.php'); //volvemos a la página 'login.php'
 }
 if (isset($_POST['detalle'])) { //o si ejecutamos el boton 'detalle'
     header('Location: detalle.php'); //nos vamos a la página 'detalle.php'
 }
-if (isset($_POST['editar'])) {
-    header("Location: editarPerfil.php");
+if (isset($_POST['editar'])) { //o si ejecutamos el boton 'editar'
+    header("Location: editarPerfil.php"); //nos vamos a la página 'editarPerfil.php'
 }
 
 //COOKIE
@@ -32,11 +24,28 @@ $aSaludoIdiomas = [
     "english" => "Welcome"
 ];
 
-if (isset($_GET['language'])) { //al elegir el idioma
+//COMPROBAMOS SI LA COOKIE YA EXISTE 
+if (isset($_COOKIE['language'])) {
+    $saludo = $aSaludoIdiomas[$_COOKIE['language']];
+} else { //SINO SE CREA (LA COOKIE POR DEFECTO ES EN ESPAÑOL)
+    /* ----COOKIE----- */
+    //creación de la cookie (su valor se pasara a 'programa.php' para identificar el idioma en el que aparecera la información)
+    //setcookie(nombre, valor, expires, path, domain, secure, options, httponly);
+    //name->nombre de la cookie
+    //valor->el valor de la cookie
+    //expires->el tiempo en que la cookie expira (0 = cuando se cierra la sesión)  [en este caso, dura un día]
+    //path->la ruta dentro del servidor en la que la cookie estará disponible
+    //domain->el (sub)dominio al que la cookie está disponible
+    //secure->[boolean] cuando es TRUE la cookie será accesible sólo a través del protocolo HTTP
+    //httponly->[boolean] cuando es TRUE la cookie será accesible sólo a través del protocolo HTTP
+    //NOTA: Si quieres mantener la misma cookie por varios archivos en diferentes directorios (como 'login.php' y 'programa.php') el path (ruta) y el domain (dominio) tienen que ser el mismo
+    setcookie("language", "spanish", time() + 60 * 60 * 24, "/proyectoDWES/proyectoTema5/LoginLogoffTema5/codigoPHP");
+    $saludo = $aSaludoIdiomas["spanish"];
+}
+
+if (isset($_GET['language'])) { //SI PULSAMOS CUALQUIER BOTON PARA ELEGIR EL IDIOMA
     setcookie("language", $_GET['language'], time() + 60 * 60 * 24, "/proyectoDWES/proyectoTema5/LoginLogoffTema5/codigoPHP");
     $saludo = $aSaludoIdiomas[$_GET['language']];
-} else {
-    $saludo = $aSaludoIdiomas["spanish"];
 }
 
 
@@ -62,6 +71,23 @@ try {
     $DescripcionUsuario = $oUsuario->T01_DescUsuario;
     $NumeroConexiones = $oUsuario->T01_NumConexiones;
     $PerfilUsuario = $oUsuario->T01_Perfil;
+
+    //POSIBLES RESPUESTAS QUE PUEDEN APARECER SEGÚN EL VALOR DE LOS CAMPOS DE LA BASE DE DATOS
+    if ($PerfilUsuario === "admin") { //PERFIL DE USUARIO
+        $salidaPerfil = "<h3><span class='respuesta'>Eres el admin</span></h3>";
+    } else {
+        $salidaPerfil = "<h3><span class='respuesta'>Eres un simple usuario</span></h3>";
+    }
+
+    if ($NumeroConexiones === '1') {
+        $salidaConex = "<h3><span class='respuesta'>¡Es la primera vez que te conectas!</span></h3>";
+    } else {
+        $fecha = new DateTime();
+        $fecha->setTimestamp($_SESSION['FechaHoraUltimaconexionAnterior']);
+        $fechaFormateada = $fecha->format("Y-m-d H:i:s");
+        $salidaConex = "<h3>Número de veces que te has conectado:<span class='respuesta'>$NumeroConexiones</span></h3>"
+                . "<h3>Última conexión: <span class='respuesta'> $fechaFormateada </span></h3>";
+    }
 } catch (PDOException $excepcionPDO) {
     echo "<p style='color:red;'>Mensaje de error: " . $excepcionPDO->getMessage() . "</p>"; //Muestra el mesaje de error
     echo "<p style='color:red;'>Código de error: " . $excepcionPDO->getCode() . "</p>"; // Muestra el codigo del error
@@ -81,35 +107,11 @@ try {
     <body>
         <div class="programa">
             <h2>USUARIO CORRECTO</h2>
-
             <h3>¡<?php echo $saludo ?> <span class="respuesta"><?php echo $DescripcionUsuario; ?></span>!</h3> <!-- SALUDO AL USUARIO (COOKIE Y DESCRIPCIÓN)-->
             <?php
-            if ($PerfilUsuario === "admin") { //PERFIL DE USUARIO
-                ?>
-                <h3><span class="respuesta">Eres el admin</span></h3>
-                <?php
-            } else {
-                ?>
-                <h3><span class="respuesta">Eres un simple usuario</span></h3>
-                <?php
-            }
-            //NÚMERO DE CONEXIONES DEL USUARIO
-            if ($NumeroConexiones === '1') {  //si es la primera vez, sale un mensaje
-                ?>
-                <h3><span class="respuesta">¡Es la primera vez que te conectas!</span></h3>
-                <?php
-            } else {
-                //si no es la primera vez, sale un mensaje mostrando el número de visitas y la fecha formateada (fecha y hora) de la conexión anterior
-                ?>
-                <h3>Número de veces que te has conectado:<span class="respuesta"><?php echo $NumeroConexiones; ?></span></h3>
-                <h3>Última conexión: <span class="respuesta"><?php
-                        $fecha = new DateTime();
-                        $fecha->setTimestamp($_SESSION['FechaHoraUltimaconexionAnterior']);
-                        $fechaFormateada = $fecha->format("Y-m-d H:i:s");
-                        echo $fechaFormateada;
-                    }
-                    ?>
-                </span></h3>      
+            echo $salidaPerfil;
+            echo $salidaConex;
+            ?>     
             <!-- FORMULARIO CON LOS BOTONES PARA IR A DETALLE.PHP O CERRAR LA SESIÓN -->
             <form class="programa" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
                 <input type="submit" name="detalle" value="Detalle"/>
